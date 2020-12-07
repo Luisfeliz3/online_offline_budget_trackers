bb2aefcffc637ad8d9188f99876b85457fce7759
Luis Feliz <Luisfeliz@gmail.com>
2020-12-07 15:10:19 -0500

    chore(db): adding db.json

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routesls
+/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
